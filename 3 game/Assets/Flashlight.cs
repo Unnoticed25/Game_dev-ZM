99ead1e8639ff8e703556d0e1d69df7284fb12a2
Unnoticed25 <sanchous20@gmail.com>
2023-01-22 02:10:35 +0300

    add flashlight's sound but need ro rework it
    DO NOT WORK!!!

--- 3 game/Assets/Flashlight.cs
+++ 3 game/Assets/Flashlight.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
+
+    // public AudioClip[] runningAudio;
     private Light Flashl;
+    private bool FlashlightOn;
+    private AudioSource audioturn;
     void Start()
     {
         Flashl = GetComponent<Light>();
+        audioturn = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -15,6 +20,16 @@ public class Flashlight : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
         {
             Flashl.enabled = !Flashl.enabled;
+            FlashlightOn = true;
+        }
+        FlashlightOn = false;
+    }
+
+
+    void FixedUpdate(){
+        if(FlashlightOn == true){
+           audioturn.enabled = !audioturn.enabled;
         }
     }
+
 }
